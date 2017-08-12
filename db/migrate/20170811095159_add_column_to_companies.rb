c46d843b1e459b6bb28cf447e3e75965998c8ba4
venky <venkykly@gmail.com>
2017-08-12 21:56:58 +0530

    company setup

--- db/migrate/20170811095159_add_column_to_companies.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnToCompanies < ActiveRecord::Migration[5.1]
-  def change
-  	add_column :companies,:confirmation_token,:string
-  	add_column :companies,:confirmed_at,:datetime
-  	add_column :companies,:confirmation_sent_at,:datetime
-  	add_column :companies,:unconfirmed_email,:string
-  end
-end
